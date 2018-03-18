677d5bb5fef666ef273f2ec8d9a6b0c28187b154
Jassi Bacha <klikster@gmail.com>
2018-03-18 14:04:48 -0700

    Ch 2 Step 7

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -6,12 +6,11 @@ class BusinessList extends React.Component {
     render() {
         return (
             <div className="BusinessList">
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
+                {
+                    this.prop.businesses.map(function(business) {
+                        <Business business="{business}" />
+                    });
+                }
             </div>
         );
     }
