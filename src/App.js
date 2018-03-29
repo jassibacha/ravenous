644775a28e334f7eca3baa315e7d7029ab980dc4
Jassi Bacha <klikster@gmail.com>
2018-03-28 18:13:14 -0700

    Ch 4 stilll broken

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
 
 import Yelp from '../src/util/Yelp';
-import logo from './logo.svg';
-
-class App extends Component {
+//import logo from './logo.svg';
+class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       businesses: []
     };
+
     this.searchYelp = this.searchYelp.bind(this);
   }
+
   searchYelp(term, location, sortBy) {
-    //console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
     Yelp.search(term, location, sortBy).then(businesses => {
       this.setState({businesses: businesses});
     });
   }
+
   render() {
+    console.log(this.state);
     return (
       <div className="App">
         <h1>ravenous</h1>
