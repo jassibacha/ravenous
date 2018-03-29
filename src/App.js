d7d918fec1fb27727275b1e7125f3a7b4995f0ef
Jassi Bacha <klikster@gmail.com>
2018-03-28 17:37:24 -0700

    Ch4 Step 23

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Yelp from '../src/util/Yelp'
 
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
 
-const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90,
-}
-const businesses = [business,business,business,business,business,business];
+// const business = {
+//   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+//   name: 'MarginOtto Pizzeria',
+//   address: '1010 Paddington Way',
+//   city: 'Flavortown',
+//   state: 'NY',
+//   zipCode: '10101',
+//   category: 'Italian',
+//   rating: 4.5,
+//   reviewCount: 90,
+// }
+// const businesses = [business,business,business,business,business,business];
 
 class App extends Component {
   searchYelp(term, location, sortBy) {
--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,7 +1,28 @@
 const apiKey = 'qdLsUMZi7gWTEkxrjP9wFw';
 
-const Yelp = {
+export const Yelp = {
     search(term, location, sortBy) {
-        return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`);
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, { 
+            headers: {
+              { Authorization: `Bearer ${apiKey}` }
+            }
+          }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => {
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.address1,
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipCode: business.location.zip_code,
+                    category: business.categories,
+                    rating: business.rating,
+                    reviewCount: business.review_count
+                });
+            }
+        });
     }
 }
